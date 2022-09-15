d9b7cc773ff23e1321518dfdc90e5ef2cf76dd47
ecekpo <ememekpo@yahoo.com>
2022-09-15 15:00:48 +0100

    Add BookList.jsx

---
+++ src/components/BookList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+const BookList = ({ title, author }) => (
+  <ul>
+    <h2>{title}</h2>
+    <p>{author}</p>
+    <button type="button">Remove books</button>
+  </ul>
+);
+
+BookList.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
+
+export default BookList;
