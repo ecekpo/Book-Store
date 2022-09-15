03466fca4b91b1dc604de13e22e809ba85b80b99
ecekpo <ememekpo@yahoo.com>
2022-09-15 15:01:18 +0100

    Add Books.jsx

---
+++ src/components/Books.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import BookList from './BookList';
+import Form from './Form';
+
+const Books = () => {
+  const [books, setBooks] = useState([{ id: '1', title: 'Things Fall Apart', author: 'By Chinwe Achebe' }]);
+
+  const addBooks = () => setBooks();
+
+  return (
+    <div>
+      {books.map((books) => (
+        <BookList
+          key={books.id}
+          title={books.title}
+          author={books.author}
+        />
+      ))}
+      <Form addBooks={addBooks} />
+    </div>
+  );
+};
+
+export default Books;
