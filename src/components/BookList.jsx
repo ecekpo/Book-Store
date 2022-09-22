288b616335ed3dcd0ea79ef01f57118b8e398c22
ecekpo <ememekpo@yahoo.com>
2022-09-22 14:09:58 +0100

    Add styling to bookstore

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,5 +1,9 @@
+import React from 'react';
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
 import { booksDeletedFromApi } from '../redux/books/books';
+import 'react-circular-progressbar/dist/styles.css';
+import '../styles/index.css';
 
 /* eslint-disable react/prop-types */
 function BookList({
@@ -7,20 +11,53 @@ function BookList({
 }) {
   const dispatch = useDispatch();
   const chapter = Math.floor(Math.random() * 11);
+  const percentage = 66;
   const handleDelete = () => {
     dispatch(booksDeletedFromApi(id));
   };
 
   return (
-    <ul>
-      <h2>{title}</h2>
-      <p>{author}</p>
-      <p>{category}</p>
-      <p>
-        Chapter:
-        {chapter}
-      </p>
-      <button type="button" onClick={handleDelete}>Remove books</button>
+    <ul className="bookScreen_container">
+      <div className="bookScreen">
+        <div className="col1">
+          <div className="col1-row1">
+            <p className="category">{category}</p>
+            <h3 className="title">{title}</h3>
+            <p className="author">{author}</p>
+          </div>
+          <div className="col1-row2">
+            <span>Comments</span>
+            <div className="span-bookScreen" />
+            <div>
+              <button type="button" onClick={handleDelete} className="book-remove-btn">
+                Remove
+              </button>
+            </div>
+            <div className="span-bookScreen" />
+            <div>Edit</div>
+          </div>
+        </div>
+        <div className="percentage">
+          <CircularProgressbar
+            className="circularprogressbar"
+            value={percentage}
+            text={`${percentage}%`}
+          />
+          <div className="percentage2">
+            <p>{`${percentage}%`}</p>
+            Completed
+          </div>
+        </div>
+        <span className="book-chapter" />
+        <div className="book_chapter">
+          <h3>CURRENT CHAPTER</h3>
+          <span>
+            Chapter
+            {chapter}
+          </span>
+          <button type="button" className="percentage-progress-btn">Update Percentage Progress</button>
+        </div>
+      </div>
     </ul>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CgProfile } from 'react-icons/cg';
 
 const Header = () => (
   <div>
     <nav>
       <h1>Bookstore CMS</h1>
-      <ul>
+      <ul className="nav-link">
         <li><Link to="/" style={{ textDecoration: 'none' }}>BOOKS</Link></li>
         <li><Link to="/categories" style={{ textDecoration: 'none' }}>CATEGORIES</Link></li>
       </ul>
+      <CgProfile className="book-avatar" />
     </nav>
   </div>
 );
